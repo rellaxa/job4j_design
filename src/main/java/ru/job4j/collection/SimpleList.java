32917b2431f5e737653f9a838bcb96a9aadb1ad8
relaxa <ewg.nadeev@yandex.ru>
2024-04-18 18:06:23 +0300

    1. Динамический список на массиве. [#158 #56099]

---
+++ src/main/java/ru/job4j/collection/SimpleList.java
@@ -0,0 +1,11 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+
+public interface SimpleList<T> extends Iterable<T> {
+    void add(T value);
+    T set(int index, T newValue);
+    T remove(int index);
+    T get(int index);
+    int size();
+}
