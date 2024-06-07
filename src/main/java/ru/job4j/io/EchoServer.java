c8a2cf5db313057fd29c72dbf31367b82f1b1117
relaxa <ewg.nadeev@yandex.ru>
2024-06-07 17:27:14 +0300

    3. Бот [#7921 #171233]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -14,14 +14,19 @@ public class EchoServer {
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                        System.out.println(string);
-                        if (string.contains("Bye")) {
-                            output.write("Server is closed!".getBytes());
-                            server.close();
-                        }
-                        if (string.contains("Hello")) {
-                            output.write("Hello, how a u doing?".getBytes());
+                    String message = null;
+                    String string = input.readLine();
+                    if (string.contains("msg=")) {
+                        message = string.substring(string.indexOf("=") + 1, string.lastIndexOf(" "));
+                    }
+                    if ("Exit".equalsIgnoreCase(message)) {
+                        output.write("Server is closed".getBytes());
+                        server.close();
+                    } else if (message != null) {
+                        if ("Hello".equalsIgnoreCase(message)) {
+                            output.write("Hello, dear friend.".getBytes());
+                        } else {
+                            output.write(message.getBytes());
                         }
                     }
                     output.flush();
